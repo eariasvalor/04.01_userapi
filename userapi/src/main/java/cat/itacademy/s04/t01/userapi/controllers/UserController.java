52ee816a50b6a6bfd31b37e69b7e3c50c1d13a68
eariasvalor <eariasvalor@gmail.com>
2025-12-04 18:46:38 +0100

    feat: add endpoint POST/users

--- userapi/src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ userapi/src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -2,24 +2,36 @@ package cat.itacademy.s04.t01.userapi.controllers;
 
 import cat.itacademy.s04.t01.userapi.entities.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class UserController {
-    private static List<User> users;
-
-    public UserController() {
-        this.users = new ArrayList<>();
-    }
+    private static List<User> users = new ArrayList<>();
 
     @GetMapping("/users")
     public static List<User> getUsers() {
         return Collections.unmodifiableList(users);
     }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User newUser){
+        UUID id = UUID.randomUUID();
+
+        User user = new User(id, newUser.getName(), newUser.getEmail());
+
+        users.add(user);
+
+        return user;
+    }
+
+
 
 
 }
